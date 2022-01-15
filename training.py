d6482f68b19e91fa6450e275b7c50df023fef623
Marc-Antoine Boyé <marc-antoine@MacBook-Air-de-Alexis.local>
2022-01-15 14:07:39 +0100

    push ft_linear_regression

--- training.py
+++ training.py
@@ -18,19 +18,47 @@ def sum_theta1(theta0, theta1, mileages, prices, m):
 		value += (hypothesis(theta0, theta1, mileages[i]) - prices[i]) * mileages[i]
 	return value
 
-def gradient_descent(mileages, prices, i):
+def gradient_descent(x, y, i):
 	theta0, theta1 = 0., 0.
-	m = len(mileages)
+	m = len(x)
 	for j in range(i):
-		tmp_t0 = sum_theta0(theta0, theta1, mileages, prices, m) / m
-		tmp_t1 = sum_theta1(theta0, theta1, mileages, prices, m) / m
+		tmp_t0 = sum_theta0(theta0, theta1, x, y, m) / m
+		tmp_t1 = sum_theta1(theta0, theta1, x, y, m) / m
 		theta0 = theta0 - learning_rate * tmp_t0
 		theta1 = theta1 - learning_rate * tmp_t1
-	return theta0 * 1000000, theta1
+	min, max = dataset_minmax(dataset)
+	return denormalize(theta0, min, max), theta1
+
+def	dataset_minmax(dataset):
+	arr1 = [row[0] for row in dataset]
+	arr2 = [row[1] for row in dataset]
+	min = 999999999
+	max = 0
+	for i in range(len(arr1)):
+		if int(arr1[i]) < min: min = arr1[i]
+		if int(arr1[i]) > max: max = arr1[i]
+	for i in range(len(arr2)):
+		if int(arr2[i]) < min: min = arr2[i]
+		if int(arr2[i]) > max: max = arr2[i]
+	return min, max
+
+def	denormalize(value, min, max):
+	return value * (max - min) + min
+
+def	normalize(value, min, max):
+	return (value - min) / (max - min)
+
+def	get_mileages(dataset):
+	min, max = dataset_minmax(dataset)
+	return [normalize(row[0], min, max) for row in dataset]
+
+def	get_prices(dataset):
+	min, max = dataset_minmax(dataset)
+	return [normalize(row[1], min, max) for row in dataset]
 
 if __name__ == "__main__":
 	dataset = load_csv("data.csv")
-	mileages = [row[0] / 1000000 for row in dataset]
-	prices = [row[1] / 1000000 for row in dataset]
-	theta0, theta1 = gradient_descent(mileages, prices, 100000)
+	mileages = get_mileages(dataset)
+	prices = get_prices(dataset)
+	theta0, theta1 = gradient_descent(mileages, prices, 10000)
 	save_in_file(theta0, theta1, 0.)
